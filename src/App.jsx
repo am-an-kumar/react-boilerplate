c85c6d795fe17379dfc409133429b8b12270f1ba
Aman <amankumar.edith@gmail.com>
2019-08-11 06:08:11 +0530

    Removed code added for testing tooling

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,15 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import('./Warning.jsx'))
-
 class App extends React.Component {
-  state = {
-    count: 0,
-  }
-
   render() {
-    const { count } = this.state
     return (
       <div>
         <h1>Hello World</h1>
-        <h2
-          style={{
-            color: count < 0 ? 'red' : count > 0 ? 'seagreen' : 'black',
-          }}
-        >
-          Count: {count}
-          {count > 10 ? (
-            <React.Suspense fallback='Hi there!!!'>
-              <Warning />
-            </React.Suspense>
-          ) : null}
-        </h2>
-        <button
-          onClick={() =>
-            this.setState(state => ({
-              count: state.count + 1,
-            }))
-          }
-        >
-          +
-        </button>
-        <button
-          onClick={() =>
-            this.setState(state => ({
-              count: state.count - 1,
-            }))
-          }
-        >
-          -
-        </button>
       </div>
     )
   }
 }
 
+// this component and its children will be hot module replaced instead of a full page refresh
 export default hot(module)(App)
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import App from './App.jsx'
 import '@babel/polyfill'
 import './css/style.css'
 
+// react-axe will run only in dev mode
 if (process.env.NODE_ENV === 'development') {
   const axe = require('react-axe')
+  // running the accessibility tests 1 second after the app is loaded
   axe(React, ReactDOM, 1000)
 }
 
